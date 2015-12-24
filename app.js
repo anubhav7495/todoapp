2df23a6b612306c7dfa21dbbf2eb8d4ebaed7992
anubhav7495 <anubhav.worklinux@gmail.com>
2015-12-24 14:09:51 +0530

    port fix2

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var express = require('./config/express'),
   mongoose = require('./config/mongoose');
 
-app.set('port', (process.env.PORT || 3000));
-
 var db = mongoose();
 var app = express();
+
+app.set('port', (process.env.PORT || 3000));
 
 app.listen(app.get('port'), function() {
   console.log('app is running on port', app.get('port'));
